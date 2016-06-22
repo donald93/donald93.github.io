2213a686af65d45a6c1b01eb19eeedb7b3b2b4d5
Donald McPartland <dmcpartland@healthequity.com>
2016-06-22 15:05:30 -0600

    Everything is set up to add new games to the games list. Just need to implement that method, and probably a service to link IDs to names

--- js/controllers/GamesListController.js
+++ js/controllers/GamesListController.js
@@ -1,5 +1,31 @@
 siteControllers.controller('GamesListController', function ($scope, httpService) {
 
+  $scope.newGamePlayersCount = 3;
+  $scope.newGame = {};
+  $scope.newGame.players = new Array ($scope.newGamePlayersCount);
+
+  // $scope.getNewGamePlayersCount = function () {
+  //   return $scope.newGame.players = new Array ($scope.newGamePlayersCount);
+  // }
+
+  $scope.incrementPlayersCount = function () {
+    $scope.newGamePlayersCount++;
+    $scope.newGame.players.length = $scope.newGamePlayersCount;
+  }
+
+  $scope.decrementPlayersCount = function () {
+    $scope.newGamePlayersCount--;
+    $scope.newGame.players.length = $scope.newGamePlayersCount;
+  }
+
+  $scope.availableToRemove = function (index) {
+    return $scope.newGamePlayersCount == index && $scope.newGamePlayersCount > 3;
+  }
+
+  $scope.reachedMaxPlayers = function () {
+    return $scope.newGamePlayersCount >= 8;
+  }
+
   var onRecentGamesComplete = function (response) {
     $scope.games = response.games;
   };
@@ -11,13 +37,18 @@ siteControllers.controller('GamesListController', function ($scope, httpService)
   httpService.getRecentGames().then(onRecentGamesComplete, onError);
 
   $scope.gamesListOrder = '-date';
-  
+
   $scope.showAddGame = false;
-  
-  var toggleAddGame = function(){
+
+  var toggleAddGame = function () {
     $scope.showAddGame = !$scope.showAddGame;
+    $scope.newGamePlayersCount = 3;
   }
-  
+
   $scope.toggleAddGame = toggleAddGame;
-  
+
+  $scope.addGame = function (game) {
+
+  }
+
 });
